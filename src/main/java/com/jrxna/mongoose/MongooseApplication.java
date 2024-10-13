574d2f0ca26413a6bba59dc316bce6e372d496c2
Joel Rego <jrxna@outlook.com>
2024-10-13 12:07:04 +0530

    feat: add resources for static site template

--- src/main/java/com/jrxna/mongoose/MongooseApplication.java
+++ src/main/java/com/jrxna/mongoose/MongooseApplication.java
@@ -3,6 +3,13 @@ package com.jrxna.mongoose;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.*;
 
 @SpringBootApplication
 public class MongooseApplication implements CommandLineRunner {
@@ -13,6 +20,8 @@ public class MongooseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Application logic will go here
+        MutableDataSet options = new MutableDataSet();
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
     }
 }
